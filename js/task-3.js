ee69e5c8fd1333f6ae4ac933e8739f263f8c3eee
melovarr <meeelnik@gmail.com>
2025-02-20 07:47:29 +0200

    get homework-5

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,38 @@
-const profile = { username: "Jacob", playTime: 300,
-changeUsername(newName) {
-    profile.username = newName;
-},
-updatePlayTime(hours) {
-    profile.playTime += hours;
-},
-getInfo() {
-    return `${profile.username} has ${profile.playTime} active hours!`;
-    },
-};
+// Напиши стрілочну функцію sortByDescendingFriendCount(users) , яка прийматиме один параметр users — масив об’єктів користувачів.
+// Функція має повертати масив усіх користувачів, відсортованих за спаданням кількостій їх друзів (властивість friends).
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+// ---------------------------------------------------------------
+const allUsers = [
+  { name: "Moore Hensley", friends: ["Sharron Pace"], gender: "male" },
+  { name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"], gender: "female" },
+  { name: "Ross Vazquez", friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"], gender: "male" },
+  { name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"], gender: "female" },
+  { name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"], gender: "male" },
+  { name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"], gender: "male" },
+  { name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"], gender: "female" }];
 
+  
+const res = allUsers.toSorted((a, b) => b.name.localeCompare(a.name));
+// ---------------------------------------------------------------
+console.log(res);
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+// console.log(
+//   sortByDescendingFriendCount([
+//     {name: "Moore Hensley", friends: ["Sharron Pace"], gender: "male"},
+//     {name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"], gender: "female"},
+//     {name: "Ross Vazquez", friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"], gender: "male"},
+//     {name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"], gender: "female"},
+//     {name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"], gender: "male"},
+//     {name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"], gender: "male"},
+//     {name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"], gender: "female"}
+//   ])
+// );
+// [
+//   {name: "Ross Vazquez", friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"], gender: "male"},
+//   {name: "Sharlene Bush", friends: ["Briana Decker", "Sharron Pace"], gender: "female"},
+//   {name: "Elma Head", friends: ["Goldie Gentry", "Aisha Tran"], gender: "female"},
+//   {name: "Carey Barr", friends: ["Jordan Sampson", "Eddie Strong"], gender: "male"},
+//   {name: "Blackburn Dotson", friends: ["Jacklyn Lucas", "Linda Chapman"], gender: "male"},
+//   {name: "Sheree Anthony", friends: ["Goldie Gentry", "Briana Decker"], gender: "female"},
+//   {name: "Moore Hensley", friends: ["Sharron Pace"], gender: "male"}
+// ]
